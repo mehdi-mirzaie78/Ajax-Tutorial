4e32fd7c940155edc9cceb26fb4244724b3ccc6c
mahdi <mehdimirzaie1378@gmail.com>
2023-09-12 16:47:36 +0330

    modified views.py & settings.py

--- todo/views.py
+++ todo/views.py
@@ -2,5 +2,4 @@ from django.shortcuts import render
 from .models import Todo
 
 def index_view(request):
-    todo = Todo.objects.last()
-    return render(request, 'index.html', {'todo': todo})
+    return render(request, 'index.html')
